c69621ddd7ffba2ae34a1f2f59a32cc31bc571cb
samueleffiongjacob <samueleffiong685@gmail.com>
2022-09-08 17:52:40 -0700

    god is good

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 		long l;
 		long long ll;
 		float f;
-	
+
 		printf("Size of a char: %d byte(s)\n", sizeof(c));
 		printf("Size of an int: %d byte(s)\n", sizeof(i));
 		printf("Size of a long int: %d byte(s)\n", sizeof(l));
