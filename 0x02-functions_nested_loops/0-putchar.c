315b45ed3a84ec0f03daa3c771acfe731b692a70
samueleffiongjacob <samueleffiong685@gmail.com>
2022-09-14 22:50:32 -0700

    ffjkjgf

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
+
 /**
- * main - Print "_putchar" followed by a new line.
- * Description: You are not allowed to include standard libraries.
+ * main - Entry point
  *
- * Return: 0
+ * Return: nothing, return void
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('p');
-	_putchar('t');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('p');
+_putchar('t');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
